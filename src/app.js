cf94875124eb38e4ac0f4044c94604da0be6fcd4
MRaflyH <raflyhendrakusuma@gmail.com>
2025-01-14 23:32:56 +0700

    verceel1!!!!

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ const app = express();
 app.use(express.json());
 app.use("/api/tasks", tasksRoute);
 
+// Default route for the root
 app.get("/", (req, res) => {
-    res.send("Task Manager API is live!");
-  });
-  
+  res.send("Task Manager API is live!");
+});
+
 module.exports = app;
