a08485e2fa1dc65e00094d967e0ddbda8755cacd
MRaflyH <raflyhendrakusuma@gmail.com>
2025-01-14 23:30:23 +0700

    verrcel?!?!?

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 3000;
-
 (async () => {
   await connectDB();
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  console.log("Database connected!");
 })();
+
+// Export the app for Vercel
+module.exports = app;
